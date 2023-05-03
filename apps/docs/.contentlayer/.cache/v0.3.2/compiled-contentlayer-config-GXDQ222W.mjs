9155b837cdab199d7d2ddbce20a47b5c06b7a0a7
Martin PAUCOT <m.paucot@syneki.com>
2023-05-03 18:31:51 +0200

    chore(docs): remove .contentlayer folder

--- apps/docs/.contentlayer/.cache/v0.3.2/compiled-contentlayer-config-GXDQ222W.mjs
+++
@@ -1,9 +0,0 @@
-// contentlayer.config.ts
-import { makeSource } from "contentlayer-source-notion";
-var contentlayer_config_default = makeSource({
-  databaseTypes: []
-});
-export {
-  contentlayer_config_default as default
-};
-//# sourceMappingURL=compiled-contentlayer-config-GXDQ222W.mjs.map
